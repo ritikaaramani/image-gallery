ac4063ba79d81fc7a4963c57dfea92ec4b0110a4
ritikaaramani <ritika.aramani06@gmail.com>
2025-09-13 20:47:22 +0530

    changes1

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true, // <-- allow building even with TS errors
   },
+  eslint: {
+    ignoreDuringBuilds: true, // <-- THIS is what you're missing!
+  },
 };
 
 export default nextConfig;
